898f79f3002a1737e7a497ce60803485bc854933
Omar Palacios <omarpal88@gmail.com>
2021-01-29 23:37:19 -0500

    Se realizan cambios en el HTML, se configuran estilos del darkmode del header, y se programa el JS con el cambio de theme con click en el seleccionador de modo

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function darkMode () {
+  document.getElementById("body").classList.toggle("darktheme");
+  document.getElementById("header").classList.toggle("darktheme");
+  document.getElementById("h1").classList.toggle("darktheme");
+  document.getElementById("header-line-separator").classList.toggle("darktheme");
+  document.getElementById("mode-selector").classList.toggle("darktheme");
+  document.getElementById("mode-mark").classList.toggle("darktheme");
+};
