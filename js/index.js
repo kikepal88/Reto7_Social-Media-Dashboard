fa391ecb39e4fe386a74b79ef971a0bd2e932b36
Omar Palacios <omarpal88@gmail.com>
2021-02-04 21:10:37 -0500

    Optimización de código JS

--- js/index.js
+++ js/index.js
@@ -1,37 +1,11 @@
-function darkMode () {
-  document.getElementById("body").classList.toggle("darktheme");
-  document.getElementById("header").classList.toggle("darktheme");
-  document.getElementById("h1").classList.toggle("darktheme");
-  document.getElementById("header-line-separator").classList.toggle("darktheme");
-  document.getElementById("mode-selector").classList.toggle("darktheme");
-  document.getElementById("mode-mark").classList.toggle("darktheme");
-  document.getElementById("article-facebook").classList.toggle("darktheme");
-  document.getElementById("article-twitter").classList.toggle("darktheme");
-  document.getElementById("article-instagram").classList.toggle("darktheme");
-  document.getElementById("article-youtube").classList.toggle("darktheme");
-  document.getElementById("article-facebook-user").classList.toggle("darktheme");
-  document.getElementById("article-twitter-user").classList.toggle("darktheme");
-  document.getElementById("article-instagram-user").classList.toggle("darktheme");
-  document.getElementById("article-youtube-user").classList.toggle("darktheme");
-  document.getElementById("article-facebook-followers").classList.toggle("darktheme");
-  document.getElementById("article-twitter-followers").classList.toggle("darktheme");
-  document.getElementById("article-instagram-followers").classList.toggle("darktheme");
-  document.getElementById("article-youtube-followers").classList.toggle("darktheme");
-  document.getElementById("overview").classList.toggle("darktheme");
-  document.getElementById("overview-cards-face1").classList.toggle("darktheme");
-  document.getElementById("overview-cards-face2").classList.toggle("darktheme");
-  document.getElementById("overview-cards-insta1").classList.toggle("darktheme");
-  document.getElementById("overview-cards-insta2").classList.toggle("darktheme");
-  document.getElementById("overview-cards-twit1").classList.toggle("darktheme");
-  document.getElementById("overview-cards-twit2").classList.toggle("darktheme");
-  document.getElementById("overview-cards-yout1").classList.toggle("darktheme");
-  document.getElementById("overview-cards-yout2").classList.toggle("darktheme");
-  document.getElementById("cards-dates-face1").classList.toggle("darktheme");
-  document.getElementById("cards-dates-face2").classList.toggle("darktheme");
-  document.getElementById("cards-dates-insta1").classList.toggle("darktheme");
-  document.getElementById("cards-dates-insta2").classList.toggle("darktheme");
-  document.getElementById("cards-dates-twit1").classList.toggle("darktheme");
-  document.getElementById("cards-dates-twit2").classList.toggle("darktheme");
-  document.getElementById("cards-dates-yout1").classList.toggle("darktheme");
-  document.getElementById("cards-dates-yout2").classList.toggle("darktheme");
+let ids = ["body", "header", "h1", "header-line-separator", "mode-selector", "mode-mark", "article-facebook", "article-twitter", "article-instagram", "article-youtube", "article-facebook-user", "article-twitter-user", "article-instagram-user", "article-youtube-user", "article-facebook-followers", "article-twitter-followers", "article-instagram-followers", "article-youtube-followers", "overview", "overview-cards-face1", "overview-cards-face2", "overview-cards-insta1", "overview-cards-insta2", "overview-cards-twit1", "overview-cards-twit2", "overview-cards-yout1", "overview-cards-yout2", "cards-dates-face1", "cards-dates-face2", "cards-dates-insta1", "cards-dates-insta2", "cards-dates-twit1", "cards-dates-twit2", "cards-dates-yout1", "cards-dates-yout2"];
+
+const addClassList = function(item_id) {
+  for (let i = 0; i < ids.length; i++) {
+    document.getElementById(item_id[i]).classList.toggle("darktheme");
+  };
 };
+
+function darkMode() {
+  addClassList(ids);
+}
